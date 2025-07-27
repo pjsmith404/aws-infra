89a9870cf46bdf7d91f87db41cf21c0dd731c1ef
pjsmith404 <pjsmith404@gmail.com>
2025-07-27 21:37:10 +1000

    Add a backend

--- main.tf
+++ main.tf
@@ -5,9 +5,20 @@ terraform {
       version = "6.5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "pjsmith404-tf-backend"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-2"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {}
+
+resource "aws_s3_bucket" "tf_backend" {
+  bucket = "pjsmith404-tf-backend"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
