d3a68d1551c37acff5e1b496afe7f7d00ff51449
pjsmith404 <pjsmith404@gmail.com>
2026-09-25 12:16:22 +1000

    Add bucket name to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "tf_backend_bucket" {
+  description = "Bucket name for TF state backend"
+  value = aws_s3_bucket.tf_backend.id
+}
+
 output "syphilicious_net_zone_id" {
   description = "The zone ID for syphilicious.net"
   value = aws_route53_zone.syphilicious_net.zone_id
